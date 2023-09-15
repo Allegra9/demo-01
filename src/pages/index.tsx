96679f1a2b75963104445293e6e1990cee65c9ce
Allegra9 <allegra.eng@gmail.com>
2023-09-15 21:19:07 +0100

    Update metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import Head from 'next/head'
 const Home = () => (
   <>
     <Head>
-      <title>Demo App</title>
-      <meta name="description" content="Demo app" />
+      <title>Image Searchy</title>
+      <meta name="Search for fun pictures" content="Image search app" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
